d361c05b14f190e8be4ce6816d166bd26f948760
Gerson David Guacho Uzho <gguacho3107@utm.edu.ec>
2026-08-01 21:55:25 -0500

    feat: Subir proyecto base del backend del help desk

---
+++ src/main/java/com/example/backendhelpdesk/BackendHelpDeskApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendhelpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendHelpDeskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendHelpDeskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backendhelpdesk/BackendHelpDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendhelpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendHelpDeskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
